511554587ccbff80facb4fcd0c5f194bf0860a67
trantrungtien18 <mailtrantrungtien@gmail.com>
2023-11-16 23:15:21 +0700

    ADD: modify GD_ThongKe

--- src/main/java/view/GD_ThongKe.java
+++ src/main/java/view/GD_ThongKe.java
@@ -6,8 +6,14 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
+import java.time.Month;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -34,6 +40,9 @@ import org.jfree.data.category.DefaultCategoryDataset;
 
 import com.toedter.calendar.JDateChooser;
 
+import connectDB.ConnectDB;
+import entity.NhanVien;
+
 public class GD_ThongKe extends JFrame implements ActionListener {
 	private JPanel pnNorth, pnCenter, pnSouth, pnInfo, pnChart;
 	private JLabel lblTitle, lblNgayBD, lblNgayKT, lblLoaiTK;
@@ -41,10 +50,18 @@ public class GD_ThongKe extends JFrame implements ActionListener {
 	private JComboBox<String> cmbLoaiTK;
 	private JButton btnThongKe, btnXoaTrang;
 	private Box box, box1, box2;
-	private ArrayList<String> listMonth;
-	private ArrayList<Integer> listScore;
+
+	private ArrayList<String> listMonthName;
+	private ArrayList<Double> listScore;
+	private ArrayList<Integer> listMonth;
+	private ArrayList<Date> listDate;
+	private ArrayList<Date> dateRange;
+	private ArrayList<String> listNhanVien;
+
+	private ConnectDB connectDB;
 
 	public GD_ThongKe() {
+		connectDB = ConnectDB.getInstance();
 		createGUI();
 	}
 
@@ -87,7 +104,7 @@ public class GD_ThongKe extends JFrame implements ActionListener {
 		box1.add(Box.createHorizontalStrut(10));
 		box1.add(cmbLoaiTK = new JComboBox());
 		cmbLoaiTK.addItem("Tất cả");
-		cmbLoaiTK.addItem("Theo ngày");
+		cmbLoaiTK.addItem("Theo thời gian");
 		cmbLoaiTK.addItem("Theo nhân viên");
 		box1.add(Box.createHorizontalStrut(20));
 
@@ -113,11 +130,7 @@ public class GD_ThongKe extends JFrame implements ActionListener {
 		pnChart.setBorder(BorderFactory.createLineBorder(Color.black));
 		pnChart.setLayout(new BorderLayout(0, 0));
 
-		listScore = new ArrayList<>();
-//		for (int i=0; i< 5; i++) {
-//			listScore.add(10);
-//		}
-//		buildChart();
+		buildChart();
 
 		btnThongKe.setBackground(new Color(107, 208, 107));
 		btnXoaTrang.setBackground(new Color(107, 208, 107));
@@ -126,14 +139,31 @@ public class GD_ThongKe extends JFrame implements ActionListener {
 		btnXoaTrang.addActionListener(this);
 	}
 
+	private void getDataTheoThang() {
+		listMonth = new ArrayList<>();
+		listScore = new ArrayList<>();
+		Connection connection = connectDB.getConnection();
+		String query = "SELECT * FROM DuLieuThongKeTheoThangView";
+		try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				listMonth.add(resultSet.getInt("Thang"));
+				listScore.add(resultSet.getDouble("TongTien"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		getMonthFromDAO();
+	}
+
 	private void buildChart() {
+		getDataTheoThang();
 		try {
-			getMonthFromDate();
 			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 			ArrayList<DefaultCategoryDataset> listDataset = new ArrayList<>();
 
-			for (int i = 0; i < listMonth.size(); i++) {
-				dataset.setValue((i + 10) * 100 + (i / 5), "Vertical", listMonth.get(i));
+			for (int i = 0; i < listMonthName.size(); i++) {
+				dataset.setValue(listScore.get(i), "Vertical", listMonthName.get(i));
 				listDataset.add(dataset);
 			}
 
@@ -150,39 +180,136 @@ public class GD_ThongKe extends JFrame implements ActionListener {
 			pnChart.validate();
 
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 	}
 
-	private void getMonthFromDate() throws Exception {
-		listMonth = new ArrayList<String>();
-		if (ngayBatDau.getDate() != null) {
-			if (ngayKetThuc.getDate() != null) {
-				Date startDate = ngayBatDau.getDate();
-				Date endDate = ngayKetThuc.getDate();
+	private void buildChartByTime() {
+		ArrayList<Date> listTime = new ArrayList<>();
+		listTime = getTimeFromDate();
+		try {
+			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+			ArrayList<DefaultCategoryDataset> listDataset = new ArrayList<>();
 
-				Calendar calendar = Calendar.getInstance();
-				calendar.setTime(startDate);
-				int startMonth = calendar.get(Calendar.MONTH);
-				calendar.setTime(endDate);
-				int endMonth = calendar.get(Calendar.MONTH);
-
-				SimpleDateFormat dateFormat = new SimpleDateFormat("MMM", Locale.ENGLISH);
-
-				for (int i = startMonth; i <= endMonth; i++) {
-					calendar.set(Calendar.MONTH, i);
-					String month = dateFormat.format(calendar.getTime());
-					listMonth.add(month);
-				}
-			} else {
-				JOptionPane.showMessageDialog(this, "Vui lòng chọn ngày kết thúc");
-				ngayKetThuc.requestFocus();
+			for (int i = 0; i < listTime.size(); i++) {
+				dataset.setValue(listScore.get(i), "Vertical", listTime.get(i));
+				listDataset.add(dataset);
 			}
-		} else {
-			JOptionPane.showMessageDialog(this, "Vui lòng chọn ngày bắt đầu");
-			ngayBatDau.requestFocus();
-		}
 
+			JFreeChart chart = null;
+			for (DefaultCategoryDataset curDateset : listDataset) {
+				chart = ChartFactory.createBarChart("", "Thời gian", "Doanh thu (VND)", dataset, PlotOrientation.VERTICAL,
+						false, true, false);
+			}
+
+			CategoryPlot p = chart.getCategoryPlot();
+			p.setRangeGridlinePaint(Color.black);
+			ChartPanel chartPanel = new ChartPanel(chart);
+			pnChart.add(chartPanel);
+			pnChart.validate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void buildChartByNhanVien() {
+		getDataByNhanVien();
+		try {
+			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+			ArrayList<DefaultCategoryDataset> listDataset = new ArrayList<>();
+
+			for (int i = 0; i < listScore.size(); i++) {
+				dataset.setValue(listScore.get(i), "Vertical", listNhanVien.get(i));
+				listDataset.add(dataset);
+			}
+
+			JFreeChart chart = null;
+			for (DefaultCategoryDataset curDateset : listDataset) {
+				chart = ChartFactory.createBarChart("", "Nhân viên", "Doanh thu (VND)", dataset, PlotOrientation.VERTICAL,
+						false, true, false);
+			}
+
+			CategoryPlot p = chart.getCategoryPlot();
+			p.setRangeGridlinePaint(Color.black);
+			ChartPanel chartPanel = new ChartPanel(chart);
+			pnChart.add(chartPanel);
+			pnChart.validate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void getMonthFromDAO() {
+		listMonthName = new ArrayList<>();
+		for (Integer curMonth : listMonth) {
+			Month month = Month.of(curMonth);
+			String monthName = month.getDisplayName(TextStyle.SHORT, Locale.getDefault());
+			listMonthName.add(monthName);
+		}
+//		System.out.println(listMonthName);
+	}
+
+	private void getDataByThoiGian() {
+		listDate = new ArrayList<>();
+		listScore = new ArrayList<>();
+		Connection connection = connectDB.getConnection();
+		String query = "SELECT NgayThanhToan, SUM(tongTien) AS TongTien FROM HoaDon GROUP BY NgayThanhToan";
+		try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				listDate.add(resultSet.getDate("NgayThanhToan"));
+				listScore.add(resultSet.getDouble("TongTien"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private ArrayList<Date> getTimeFromDate() {
+		getDataByThoiGian();
+		Date start = ngayBatDau.getDate();
+		Date end = ngayKetThuc.getDate();
+
+		getDateRange(start, end);
+		ArrayList<Date> newListDate = new ArrayList<>();
+
+		for (Date date2 : listDate) {
+			for (Date date : dateRange) {
+				if (date.getTime() == date2.getTime()) {
+					newListDate.add(date);
+				}
+			}
+		}
+//		System.out.println(newListDate);
+		return newListDate;
+	}
+
+	private void getDateRange(Date dateStart, Date dateEnd) {
+		dateRange = new ArrayList<>();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dateStart);
+		while (calendar.getTime().before(dateEnd)) {
+			dateRange.add(calendar.getTime());
+			calendar.add(calendar.DATE, 1);
+		}
+	}
+
+	private void getDataByNhanVien() {
+		listNhanVien = new ArrayList<>();
+		listScore = new ArrayList<>();
+		Connection connection = connectDB.getConnection();
+		String query = "SELECT HoaDon.maNhanVien, NhanVien.ten, SUM(tongTien) AS TongTien FROM HoaDon JOIN NhanVien ON HoaDon.maNhanVien = NhanVien.maNhanVien GROUP BY HoaDon.maNhanVien, NhanVien.ten";
+		try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				listNhanVien.add(resultSet.getString("ten"));
+				listScore.add(resultSet.getDouble("TongTien"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void clearInput() {
@@ -191,11 +318,22 @@ public class GD_ThongKe extends JFrame implements ActionListener {
 		cmbLoaiTK.setSelectedIndex(0);
 	}
 
+	private void xuLyThongKe() {
+		if (cmbLoaiTK.getSelectedIndex() == 0) {
+			buildChart();
+		} else if (cmbLoaiTK.getSelectedIndex() == 1) {
+			getTimeFromDate();
+			buildChartByTime();
+		}else if (cmbLoaiTK.getSelectedIndex() == 2) {
+			buildChartByNhanVien();
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object o = e.getSource();
 		if (o.equals(btnThongKe)) {
-			buildChart();
+			xuLyThongKe();
 		} else if (o.equals(btnXoaTrang)) {
 			clearInput();
 		}
