ed5d1957e7d4a82fd3792bdd32445a7bf815d41a
huynhtannha2003 <127366885+huynhtannha2003@users.noreply.github.com>
2024-04-24 01:41:58 +0700

    Update HoaDon.java

--- src/main/java/entity/HoaDon.java
+++ src/main/java/entity/HoaDon.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HoaDon {
+    private static final long serialVersionUID = 1L;
     private String maHoaDon;
     private double tongTien;
     private Date ngayThanhToan;
